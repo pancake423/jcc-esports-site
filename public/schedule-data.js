68014867f1daf346ea49b3cfe2aa17f153ad6070
pancake423 <58053103+pancake423@users.noreply.github.com>
2022-02-14 12:03:16 -0500

    added some schedule data.

--- public/schedule-data.js
+++ public/schedule-data.js
@@ -22,21 +22,45 @@ rocketleague, 4/11/2022, Johnston CC, 0, Blinn Bucaneers, 0;
 overwatch, header, JCC Overwatch (0-0);
 overwatch, label, Regular Season;
 overwatch, 2/18/2022, Johnston CC, 0, Grays Harbor College, 0;
-overwatch, 2/25/2022, Johnston CC, , BYE, ;
-overwatch, 3/4/2022, Johnston CC, 0, MBCC Bucaneers, 0;
-overwatch, 3/11/2022, Johnston CC, 0, Wake Tech. Blue, 0;
-overwatch, 3/18/2022, Johnston CC, 0, Alpena CC, 0;
-overwatch, 3/25/2022, Johnston CC, 0, Madison College WolfPack, 0;
-overwatch, 4/1/2022, Johnston CC, 0, Wake Tech. Gold, 0;
+overwatch, 2/25/2022, Johnston CC, 0, Hudson Valley CC, 0;
+overwatch, 3/4/2022, Johnston CC, 0, Finger Lakes CC Blue, 0;
+overwatch, 3/11/2022, Johnston CC, 0, Northern Virginia CC, 0;
+overwatch, 3/18/2022, Johnston CC, , BYE, ;
+overwatch, 3/25/2022, Johnston CC, 0, Laramie County CC, 0;
+overwatch, 4/1/2022, Johnston CC, 0, Suffolk CC, 0;
 overwatch, 4/08/2022, Johnston CC, 0, Jackson College: Jets, 0;
-overwatch, 4/15/2022, Johnston CC, 0, Blinn Bucaneers, 0;
+overwatch, 4/15/2022, Johnston CC, 0, Dallas College North Lake, 0;
 
 rainbowsix, header, JCC Rainbow Six Siege (0-0);
 rainbowsix, label, Regular Season;
 
 
 smash, header, JCC Smash;
+
+smash, header, Nicholas C (0-0);
 smash, label, Regular Season;
+smash, 2/16/2022, Johnston CC, 0, Fort Scott CC: Ryan H, 0;
+smash, 2/23/2022, Johnston CC, 0, Centralia College: Chance H, 0;
+smash, 3/02/2022, Johnston CC, 0, Union County College: Kevin S, 0;
+smash, 3/09/2022, Johnston CC, 0, Waubonsee Chiefs: Jacob L, 0;
+smash, 3/16/2022, Johnston CC, , BYE, ;
+smash, 3/23/2022, Johnston CC, 0, BSC Bobcats VA: Elijah W, 0;
+smash, 3/30/2022, Johnston CC, 0, BSC Bobcats VA: Camren M, 0;
+smash, 4/06/2022, Johnston CC, 0, Mineral Area College: Kody W, 0;
+smash, 4/13/2022, Johnston CC, 0, Lincoln Land Loggers: Zach M, 0;
+
+smash, header, Justin R (0-0);
+smash, label, Regular Season;
+smash, 2/16/2022, Johnston CC, 0, Grays Harbor College: Brenden L, 0;
+smash, 2/23/2022, Johnston CC, , BYE, ;
+smash, 3/02/2022, Johnston CC, 0, Heartland CC: Dalton C, 0;
+smash, 3/09/2022, Johnston CC, 0, Waubonsee Chiefs: Alex L, 0;
+smash, 3/16/2022, Johnston CC, 0, Eastern Arizona College: Alvaro B, 0;
+smash, 3/23/2022, Johnston CC, 0, Howard CC Dragons: Wyatt C, 0;
+smash, 3/30/2022, Johnston CC, 0, Arizona Western College: Nick F, 0;
+smash, 4/06/2022, Johnston CC, 0, Dallas College Richland: Jaime C, 0;
+smash, 4/13/2022, Johnston CC, 0, Montgomery County CC: Carlos P, 0;
+
 `;
 
 const ROSTERS =
